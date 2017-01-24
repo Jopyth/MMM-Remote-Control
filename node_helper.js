bb417106f62874231f0833fc020da6eed0ff336d
Chris van Marle <qistoph@gmail.com>
2017-01-24 09:58:36 +0100

    Return error on UPDATE with unknown module

--- node_helper.js
+++ node_helper.js
@@ -711,15 +711,18 @@ module.exports = NodeHelper.create({
 		var path = __dirname + "/../../";
 		var name = "MM";
 
-		if (module) {
+		if (module !== undefined && module !== 'undefined') {
 			if(self.modulesAvailable) {
-				for (var i = 0; i < self.modulesAvailable.length; i++) {
-					if (self.modulesAvailable[i].longname === module) {
-						path = __dirname + "/../" + self.modulesAvailable[i].longname;
-						name = self.modulesAvailable[i].name;
-						break;
+				var modData = self.modulesAvailable.find(m => m.longname === module);
+				if (modData === undefined) {
+					if (res) {
+						res.send({"status": "error", "reason": "unknown module", "info": module});
 					}
+					return;
 				}
+
+				path = __dirname + "/../" + modData.longname;
+				name = modData.name;
 			}
 		}
 
