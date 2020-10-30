3cad33545a0b006d5f9c03c0cdba4518a3cea9b4
Gaëtan Trellu (goldyfruit) <gaetan.trellu@ibm.com>
2020-10-30 10:51:05 -0400

    [installer.sh] Auto-detect node binary

--- installer.sh
+++ installer.sh
@@ -145,7 +145,8 @@ else
 fi
 
 # Get an UUID to use as an API key
-APIKEY=$(/usr/local/bin/node -e 'console.log(require("uuid/v4")().replace(/-/g, ""));');
+NODE_BIN=$(which node)
+APIKEY=$($NODE_BIN -e 'console.log(require("uuid/v4")().replace(/-/g, ""));');
 
 echo ""
 if check_no "Do you want to view instructions on how to configure the module?"; then
