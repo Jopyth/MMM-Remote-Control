904f1576926f6ca673740c54a57e7b51e1525b5e
Ezequiel Holzweissig <ezequielgh1999@gmail.com>
2020-12-28 21:53:41 -0300

    Bugfix #242
    Fixes #242
    Fixes #235

--- MMM-Remote-Control.js
+++ MMM-Remote-Control.js
@@ -131,12 +131,20 @@ Module.register("MMM-Remote-Control", {
             if (payload.force) { options.force = true; }
             let modules = []
             if(payload.module !== 'all') {
+                if (!Array.prototype.find) {
+                    // https://tc39.github.io/ecma262/#sec-array.prototype.find
+                    Object.defineProperty(Array.prototype,"find",{value:function(r){if(null==this)throw new TypeError('"this" is null or not defined');var e=Object(this),t=e.length>>>0;if("function"!=typeof r)throw new TypeError("predicate must be a function");for(var n=arguments[1],i=0;i<t;){var o=e[i];if(r.call(n,o,i,e))return o;i++}},configurable:!0,writable:!0});
+                }
                 let i = MM.getModules().find(m => {
-                    return (payload.module.includes(m.identifier));
+                	if(m) {
+                		return (payload.module.includes(m.identifier));
+                	}
                 });
                 if (!i) {
                     modules = MM.getModules().filter(m => {
-                        return (payload.module.includes(m.name));
+                    	if(m) {
+                        	return (payload.module.includes(m.name));
+                        }
                     });
                 } else modules.push(i)
             } else {
