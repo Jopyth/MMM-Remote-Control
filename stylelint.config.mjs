8b48b40e82e2507e365066af64e1874058f3f7fc
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2025-03-11 18:36:50 +0100

    Simplify stylelint-prettier config

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,10 +1,8 @@
 const config = {
-  "extends": ["stylelint-config-standard"],
-  "plugins": ["stylelint-prettier"],
+  "extends": ["stylelint-config-standard", "stylelint-prettier/recommended"],
   "root": true,
   "rules": {
-    "no-descending-specificity": null,
-    "prettier/prettier": true
+    "no-descending-specificity": null
   }
 };
 
