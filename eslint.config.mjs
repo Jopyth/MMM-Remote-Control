a8b7e2a8a5b89d97b38c135a0f4f0dd813783b4b
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2025-04-13 22:48:19 +0200

    chore: adjust ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,9 +4,9 @@ import globals from "globals";
 import {flatConfigs as importConfigs} from "eslint-plugin-import-x";
 
 const config = [
-  importConfigs.recommended,
   eslintPluginJs.configs.recommended,
   eslintPluginStylistic.configs.all,
+  importConfigs.recommended,
   {
     "ignores": ["**/*.min.js"]
   },
@@ -40,7 +40,6 @@ const config = [
       "no-await-in-loop": "off",
       "no-inline-comments": "off",
       "no-magic-numbers": "off",
-      "no-plusplus": "off",
       "no-var": "error",
       "one-var": "off",
       "prefer-const": "error",
@@ -59,11 +58,9 @@ const config = [
       "sourceType": "module"
     },
     "rules": {
-      "@stylistic/array-element-newline": "off",
+      "@stylistic/array-element-newline": ["error", "consistent"],
       "@stylistic/indent": ["error", 2],
-      "max-lines-per-function": ["error", 100],
-      "no-magic-numbers": "off",
-      "one-var": "off"
+      "no-magic-numbers": "off"
     }
   }
 ];
