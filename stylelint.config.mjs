c4ad8f95af7d5dffafd9a3285fca01a3caa2be81
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2025-04-23 22:30:51 +0200

    refactor: reorder #alert styles to remove rule from stylelint config

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,9 +1,7 @@
 const config = {
   "extends": ["stylelint-config-standard", "stylelint-prettier/recommended"],
   "root": true,
-  "rules": {
-    "no-descending-specificity": null
-  }
+  "rules": {}
 };
 
 export default config;
