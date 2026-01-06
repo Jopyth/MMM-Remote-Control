ba3c0583c7d7cf86e873bf2e86cca678c0ae86ba
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2026-01-06 11:43:16 +0100

    chore: switch changelog config format

---
+++ changelog.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "types": [
+    {type: "feat", section: "Added", hidden: false},
+    {type: "fix", section: "Fixed", hidden: false},
+    {type: "perf", section: "Performance Improvements", hidden: false},
+    {type: "docs", section: "Documentation", hidden: false},
+    {type: "chore", section: "Chores", hidden: false},
+    {type: "refactor", section: "Code Refactoring", hidden: false},
+    {type: "test", section: "Tests", hidden: false},
+    {type: "build", section: "Build System", hidden: false},
+    {type: "ci", section: "Continuous Integration", hidden: false}
+  ],
+  "scripts": {
+    "postchangelog": "node scripts/update-service-worker.js && git add service-worker.js"
+  }
+};
